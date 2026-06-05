222e7f91a178ed3caefa4bad5b8d53ca4f4821e3
cridrive66 <cristiano.guidi2@uni-rostock.de>
2026-06-05 17:06:33 +0200

    refining code style
    improve readability, remove blank space, remove unused variables, etc.

--- __init__.py
+++ __init__.py
@@ -34,7 +34,5 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :param iface: A QGIS interface instance.
     :type iface: QgsInterface
     """
-    #
     from .APRIORA import APRIORAPlugin
-    
     return APRIORAPlugin(iface)
